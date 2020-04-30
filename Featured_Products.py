3522a4ddfb12b012ac2da16501d57b6b74a15730
Megha1005 <babariyamegha@mail.sfsu.edu>
2020-04-30 14:03:23 -0700

    Add solution for Featured Products

--- Featured_Products.py
+++ Featured_Products.py
@@ -1,9 +1,4 @@
-
-# coding: utf-8
-
-# In[73]:
-
-
+# Hackerrank problem
 def featuredProduct(products):
     sorted_words=(sorted(products,reverse=True))
     print(sorted_words)
@@ -11,26 +6,15 @@ def featuredProduct(products):
     return max_count
 
 
-# In[64]:
-
-
 featuredProduct(['redShirt','greenPants','redShirt','orangeShoes','blackPants','blackPants'])
-
-
-# In[65]:
-
+# Expected outcome = 'redShirt'
 
 featuredProduct(['yellowShirt','redHat','blackShirt','bluePants','redHat','pinkHat','blackShirt','yellowShirt','greenPants','greenPants'])
-
-
-# In[66]:
-
+# Expected outcome = 'yelowShirt'
 
 featuredProduct(['greenShirt','bluePants','redShirt','blackShoes','redPants','redPants','whiteShirt','redShirt'])
-
-
-# In[75]:
-
+# Expected outcome = 'redShirt'
 
 featuredProduct(['a'])
+# Expected outcome = 'a'
 
